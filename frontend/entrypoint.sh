a1a146da739e02dbbdc26b1fec94ce390134b037
“Mebu98” <merijn.buitelaar98@gmail.com>
2025-02-04 16:06:44 +0100

    Photos API should now be correct, forgot to change entrypoint from Vue to Vite.

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,15 +3,17 @@
 # Code more or less from https://moreillon.medium.com/environment-variables-for-containerized-vue-js-applications-f0aa943cb962
 # And modified for this project.
 
-ROOT_DIR=/home/node/app/dist
+ROOT_DIR=/app/dist
 
-PLACEHOLDER='__VUE_APP_API_BASE_URL__'
+PLACEHOLDER='__VITE_APP_API_BASE_URL__'
 
-echo "looking for '$PLACEHOLDER' to replace with '${VUE_APP_API_BASE_URL}'"
-for file in $ROOT_DIR/js/*.js* $ROOT_DIR/index.html;
+echo "looking for '$PLACEHOLDER' to replace with '${API_BASE_URL}'"
+for file in $ROOT_DIR/assets/*.js* $ROOT_DIR/index.html;
 do
   echo "Checking $file..."
-  sed -i 's|'$PLACEHOLDER'|'${VUE_APP_API_BASE_URL}'|g' $file
+  sed -i 's|'$PLACEHOLDER'|'${API_BASE_URL}'|g' $file
 done
+
+echo "Done checking for swaps, continuing Docker."
 # Let container execution proceed
 exec "$@"
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 const apiPort = import.meta.env.VUE_APP_API_PORT || 5050;
 
 export const imgKitEndpoint = "https://ik.imagekit.io/1gxqoihjm/";
-export const apiUrl = import.meta.env.VUE_APP_API_BASE_URL || "http://localhost:" + apiPort;
+export const apiUrl = import.meta.env.VITE_APP_API_BASE_URL || "http://localhost:" + apiPort;
